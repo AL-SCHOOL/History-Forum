0f29914fd9b2a89a3b20ec424a63aae562ce8ab5
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-03-23 09:32:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ function showMessages() {
 function sendMessage() {
   var writeMessage = document.getElementById("message").value;
   var fs = require('fs');
-  fs.writeFile('messages.txt', writeMessage, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+  fs.writeFile('messages.txt', writeMessage)
 }
 
 showMessages()
